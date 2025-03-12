3eae040097a7bdde53f7a86cee95d118faea77b8
noeigenschaften <36310815+noeigenschaften@users.noreply.github.com>
2025-03-12 05:53:27 +0100

    Initial commit

---
+++ utils.py
@@ -0,0 +1,37 @@
+import re
+import os
+from google.cloud import storage
+
+#Extracts the sensing time from Sentinel-2 filename
+def extract_sensing_time(filename):
+    match = re.search(r'_(\d{8}T\d{6})_', filename)  #Match YYYYMMDDTHHMMSS between underscores
+    if match:
+        timestamp = match.group(1)
+        return f"{timestamp[:4]}-{timestamp[4:6]}-{timestamp[6:8]} {timestamp[9:11]}:{timestamp[11:13]}:{timestamp[13:]}"
+    return None  #If no timestamp found
+
+def create_gcs_bucket(bucket_name):
+    storage_client = storage.Client() #Create a client
+    bucket = storage_client.bucket(bucket_name) #Create a bucket object with the desired name
+    new_bucket = storage_client.create_bucket(bucket, location="EU") #Create the bucket located on servers in the EU region
+    print(f"Bucket {new_bucket.name} created in {new_bucket.location}.")
+    return new_bucket
+
+#Upload folder to GCS bucket
+def gcs_upload_folder(bucket_name, source, destination_blob):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    bucket.storage_class = "STANDARD"  #Free tier
+
+    for root, _, files in os.walk(source): #Scan the folder for all files
+        for file in files:
+            local_file_path = os.path.join(root, file)
+
+            #Preserve folder structure in GCS
+            relative_path = os.path.relpath(local_file_path, source)
+            destination_blob_name = os.path.join(destination_blob, relative_path)
+
+            blob = bucket.blob(destination_blob_name)
+            blob.upload_from_filename(local_file_path)
+
+            print(f"Uploaded {local_file_path} to gs://{bucket_name}/{destination_blob_name}")
